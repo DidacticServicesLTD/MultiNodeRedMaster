7b91a519c9ac325a9441d06995a6d022877bab5d
Calum Knott <calum@calumk.com>
2023-03-28 16:57:14 +0100

    Windows update

--- WINDOWS_RUN_ME_TO_UNINSTALL_EVERYTHING.bat
+++ WINDOWS_RUN_ME_TO_UNINSTALL_EVERYTHING.bat
@@ -1,11 +1,11 @@
 start "node-red-install" rmdir /s /q node_modules
-start "node-red-0-install" /D data/data_0 rmdir /s /q node_modules
-start "node-red-1-install" /D data/data_1 rmdir /s /q node_modules
-start "node-red-2-install" /D data/data_2 rmdir /s /q node_modules
-start "node-red-3-install" /D data/data_3 rmdir /s /q node_modules
-start "node-red-4-install" /D data/data_4 rmdir /s /q node_modules
-start "node-red-5-install" /D data/data_5 rmdir /s /q node_modules
-start "node-red-6-install" /D data/data_6 rmdir /s /q node_modules
-start "node-red-7-install" /D data/data_7 rmdir /s /q node_modules
-start "node-red-8-install" /D data/data_8 rmdir /s /q node_modules
-start "node-red-9-install" /D data/data_9 rmdir /s /q node_modules
+start "node-red-0-install" /D data\data_0 rmdir /s /q node_modules
+start "node-red-1-install" /D data\data_1 rmdir /s /q node_modules
+start "node-red-2-install" /D data\data_2 rmdir /s /q node_modules
+start "node-red-3-install" /D data\data_3 rmdir /s /q node_modules
+start "node-red-4-install" /D data\data_4 rmdir /s /q node_modules
+start "node-red-5-install" /D data\data_5 rmdir /s /q node_modules
+start "node-red-6-install" /D data\data_6 rmdir /s /q node_modules
+start "node-red-7-install" /D data\data_7 rmdir /s /q node_modules
+start "node-red-8-install" /D data\data_8 rmdir /s /q node_modules
+start "node-red-9-install" /D data\data_9 rmdir /s /q node_modules
